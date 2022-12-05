7b06f0190c69c25e616d3581b7757d8e2ced6ff8
Jianming <jgeng@ucsd.edu>
2022-12-05 13:17:55 -0800

    update make json file

--- make_json.py
+++ make_json.py
@@ -2,7 +2,7 @@ import json
 
 temp = {
     "dockerhub-id": "jgeng99/test_env:latest",
-    "build-script": "python run.py -t -e 10"
+    "build-script": "./run.py -te -e 10"
 }
 
 with open("submission.json", "w") as outfile:
